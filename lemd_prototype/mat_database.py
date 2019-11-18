db95c081e95950404a0acec2aeedced3d07503ea
Ninghai <nsu@lifeeusa.com>
2019-11-18 11:01:17 -0700

    Test MongoDB file storage and retrieve

--- lemd_prototype/mat_database.py
+++ lemd_prototype/mat_database.py
@@ -1,7 +1,31 @@
 from lemd_prototype.extensions import mongo
+import gridfs
 
 
 def feed_content():
     """List all content of the Mongo Database"""
 
-    return mongo.fs.files.find()
+    return ''.join([str(q['_id']) for q in mongo.db.fs.files.find()])
+
+
+class DpField:
+    _system = ''
+    _dist_to_ref = 0
+    _ids = ''
+    _version = ''
+    _file_idx = ''
+
+    def __init__(self, mat_sys :str, vers: str):
+        self._system = mat_sys
+        self._version = vers
+        db_ids = self._system + '_' + self._version
+        try:
+            self._ids = mongo.db.fs.files.find({"name": db_ids})
+        except len(self._ids) == 0:
+            raise ValueError("Given system not found in database")
+
+    def get_dist(self):
+        return self._ids["dist"]
+
+    def get_fileid(self):
+        return self._ids["_id"]
