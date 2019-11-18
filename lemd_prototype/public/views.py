507b2f4a586fb45cf8d8ccc79bef3a14546a2603
Ninghai <nsu@lifeeusa.com>
2019-11-18 13:18:14 -0700

    mongotest added

--- lemd_prototype/public/views.py
+++ lemd_prototype/public/views.py
@@ -92,10 +92,13 @@ def about():
     return render_template("public/about.html", form=form)
 
 
+@blueprint.route("/testfile/<file_id>")
 @blueprint.route("/testfile/")
 def testfile(file_id=None):
     """test MongoDB"""
     file = DpField('Si', 'v1')
-    file_id = str(file.get_dist())
+    if file_id is not None:
+        f = get_file(file_id)
+        return make_response(f.read())
 
-    return render_template("mongotest.html", file=file_id)
+    return render_template("testfile.html", file=str(file.get_fileid()))
