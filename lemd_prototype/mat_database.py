13844702ae5c8fe2d67d2a266ac00e30fd9077f2
mogudaxia <nsu@lifeeusa.com>
2019-11-18 18:06:01 +0000

    small changes for easy deployment

--- lemd_prototype/mat_database.py
+++ lemd_prototype/mat_database.py
@@ -3,5 +3,8 @@ from lemd_prototype.extensions import mongo
 
 def feed_content():
     """List all content of the Mongo Database"""
+#    res = ''
+#    for q in mongo.db.fs.files.find():
+#        res += str(q)
+    return ''.join([str(q) for q in mongo.db.fs.files.find()])
 
-    return mongo.fs.files.find()
